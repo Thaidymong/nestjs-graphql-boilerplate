6f203095e0023d7026424bb3b18b2afc48fa768a
Thai Dymong <dt8393430@gmail.com>
2024-08-18 18:11:44 +0700

    fix(change getme query)

--- src/auth/resolver/auth.resolver.ts
+++ src/auth/resolver/auth.resolver.ts
@@ -1,35 +1,38 @@
-import { Args, Mutation, Query, Resolver } from "@nestjs/graphql";
-import { AuthService } from "../auth.service";
-import { LoginResponse, RefreshTokenResponse } from "../dto/response/login.response";
-import { LoginDto } from "../dto/input/login.input";
-import { Public } from "../decorators/public.decorator";
-import { CreateUserResponse } from "src/user/dto/response";
-import { JwtAuthGuard } from "../guard/jwt-auth.guard";
-import { UseGuards } from "@nestjs/common";
-import { CurrentUser } from "../decorators";
-import { RefreshTokenDto } from "../dto/input/refresh-token.input";
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { AuthService } from '../auth.service';
+import {
+  LoginResponse,
+  RefreshTokenResponse,
+} from '../dto/response/login.response';
+import { LoginDto } from '../dto/input/login.input';
+import { Public } from '../decorators/public.decorator';
+import { CreateUserResponse, UserResponse } from 'src/user/dto/response';
+import { JwtAuthGuard } from '../guard/jwt-auth.guard';
+import { UseGuards } from '@nestjs/common';
+import { CurrentUser } from '../decorators';
+import { RefreshTokenDto } from '../dto/input/refresh-token.input';
 
 @Resolver()
 export class AuthResolver {
-    constructor(private readonly authService: AuthService) { }
+  constructor(private readonly authService: AuthService) {}
 
-    @Public()
-    @Mutation(() => LoginResponse)
-    async login(@Args('input') input: LoginDto) {
-        return await this.authService.login(input);
-    }
+  @Public()
+  @Mutation(() => LoginResponse)
+  async login(@Args('input') input: LoginDto) {
+    return await this.authService.login(input);
+  }
 
-    @Public()
-    @Mutation(() => RefreshTokenResponse)
-    async refreshToken(
-        @Args('input') input: RefreshTokenDto,
-    ): Promise<RefreshTokenResponse> {
-        return await this.authService.refreshToken(input);
-    }
+  @Public()
+  @Mutation(() => RefreshTokenResponse)
+  async refreshToken(
+    @Args('input') input: RefreshTokenDto,
+  ): Promise<RefreshTokenResponse> {
+    return await this.authService.refreshToken(input);
+  }
 
-    @Query(() => CreateUserResponse)
-    @UseGuards(JwtAuthGuard)
-    async me(@CurrentUser() user: CreateUserResponse) {
-        return user;
-    }
+  @Query(() => UserResponse)
+  @UseGuards(JwtAuthGuard)
+  async me(@CurrentUser() user: UserResponse) {
+    return user;
+  }
 }
